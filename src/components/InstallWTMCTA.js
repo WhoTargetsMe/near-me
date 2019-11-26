f060cafe7cf2638ffbc34121f73363ddbb677610
Steven Frostwick <steven@lostmango.io>
2019-11-26 11:53:06 +0000

    Update CTA text

--- src/components/InstallWTMCTA.js
+++ src/components/InstallWTMCTA.js
@@ -6,9 +6,11 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  text-align: center;
 `
 const StyledButton = styled(Button)`
   &.btn {
+    margin: 2rem 0;
     max-width: 75%;
     white-space: normal;
   }
@@ -16,12 +18,21 @@ const StyledButton = styled(Button)`
 
 const InstallWTMCTA = () => (
   <Container>
+    <p>
+      We have a chance to bring more transparency to British Elections. We're on
+      a mission to crowdsource a database of every political Facebook ad this
+      election. Now we need your help.
+    </p>
     <StyledButton
       href="https://whotargets.me/en/how-who-targets-me-works/"
       target="_parent"
     >
-      🚀 Install the Who Targets Me extension
+      🚀 Get involved — download our browser extension
     </StyledButton>
+    <p>
+      It's simple and safe, and the anonymous data you collect will help build a
+      clearer picture of how politicians are targeting all of us.
+    </p>
   </Container>
 )
 
